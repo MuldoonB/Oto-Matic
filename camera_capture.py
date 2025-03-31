e8ec5a76a9cd6b6f39a55861a067150c47b198b6
MuldoonB <56327057+MuldoonB@users.noreply.github.com>
2025-03-31 17:06:28 -0400

    Update camera_capture.py

--- camera_capture.py
+++ camera_capture.py
@@ -3,11 +3,12 @@ from picamera2 import Picamera2
 from datetime import datetime
 from signal import pause
 import os
+import time
 
 button = Button(26)
 camera =Picamera2()
 path_var= os.environ.get("GLOBAL_SES_PATH")
-
+camera.start()
 
 def capture():
     if path_var:
@@ -15,13 +16,18 @@ def capture():
     else:
         print("Path variable not set."  )  
         path_var='/home/pi/photos'
-    
+            
+
+    time.sleep(2)
     filename=f"{path_var}/photo_{datetime.now():%Y-%m-%d-%H-%M-%S}.png"
     camera.capture_file(filename,format="png")
     
-while True:
-    button.wait_for_press()  # Blocks until the button is pressed
+def button_pressed():
     capture()
     print("Ready for the next press...")
-    # Optional: Add a small delay to avoid rapid triggers if the button bounces
-    time.sleep(1)
+    time.sleep(0.2) # Debounce delay (adjust as needed)
+
+button.when_pressed = button_pressed
+
+print("Waiting for button press...")
+pause() # Efficiently wait for button presses
