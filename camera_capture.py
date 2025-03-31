0d36b9c149d3fbd1f290d3e6bd85eeef1524470b
MuldoonB <56327057+MuldoonB@users.noreply.github.com>
2025-03-31 16:33:56 -0400

    Update camera_capture.py

--- camera_capture.py
+++ camera_capture.py
@@ -7,13 +7,13 @@ import os
 button = Button(26)
 camera =Picamera2()
 path_var= os.environ.get('GLOBAL_SES_PATH')
-print(f"Path variable: {path_variable}"
+print(f"Path variable: {path_variable}")
 
 def capture():
     if path_variable:
         print(f"Path variable: {path_variable}")
     else:
-        print("Path variable not set."    
+        print("Path variable not set." )   
     
     filename=f"{path_var}/photo_{datetime.now():%Y-%m-%d-%H-%M-%S}}.png")
     camera.capture_file(filename,format="png")
